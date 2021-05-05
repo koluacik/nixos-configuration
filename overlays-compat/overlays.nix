b9eafa83618b801544bace876b571940030e8ff0
Deniz Koluaçık <koluacik@disroot.org>
2021-05-05 15:52:58 +0300

    fix xmonad overlay for real this time

---
+++ overlays-compat/overlays.nix
@@ -0,0 +1,6 @@
+self: super:
+with super.lib;
+let
+  overlays = (import <nixpkgs/nixos> { }).config.nixpkgs.overlays;
+in
+  foldl' (flip extends) (_: super) overlays self
--- overlays/xmonad-overlay.nix
+++ overlays/xmonad-overlay.nix
@@ -1,18 +1,28 @@
-self: super: {
-  haskellPackages = super.haskellPackages.extend
-  (super.haskell.lib.packageSourceOverrides {
-    xmonad = super.fetchFromGitHub {
-      owner = "xmonad";
-      repo = "xmonad";
-      rev = "a90558c07e3108ec2304cac40e5d66f74f52b803";
-      sha256 = "1swn4lfdvbancc2vqlidprr8lnllq9cwqiknri5q9ikg4n2clc7r";
-    };
+self: super:
 
-    xmonad-contrib = super.fetchFromGitHub {
-      owner = "xmonad";
-      repo = "xmonad-contrib";
-      rev = "ebf9561d762051b7f82b47242d7b30337662dc37";
-      sha256 = "11p3x469yy2c9q3vir600s21lmb1vaki1qc9pgb7vm900l0cr3ym";
-    };
+let
+
+  xmonadGH = super.fetchFromGitHub {
+    owner = "xmonad";
+    repo = "xmonad";
+    rev = "131fd3669f6c2952d3094016d14873fdfe66f98c";
+    sha256 = "1swn4lfdvbancc2vqlidprr8lnllq9cwqiknri5q9ikg4n2clc7r";
+  };
+
+  xmonad-contribGH = super.fetchFromGitHub {
+    owner = "xmonad";
+    repo = "xmonad-contrib";
+    rev = "a99c76cce48ce70cf30094a3a74f54bcb03eb3d7";
+    sha256 = "11p3x469yy2c9q3vir600s21lmb1vaki1qc9pgb7vm900l0cr3ym";
+  };
+
+in
+
+{
+  haskellPackages = super.haskellPackages.override (old: {
+    overrides = super.lib.composeExtensions (old.overrides or (_: _: {})) (hself: hsuper: rec {
+      xmonad = hself.callCabal2nix "xmonad" xmonadGH { };
+      xmonad-contrib = hself.callCabal2nix "xmonad-contrib" xmonad-contribGH { };
+    });
   });
 }
