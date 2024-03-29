a7053cd2fdd7237c4b7a8f1f4364f6a78590744a
Deniz Koluacik <koluacik@disroot.org>
2024-03-29 14:08:12 +0300

    bump flake.lock

--- modules/home-modules/services/gpg.nix
+++ modules/home-modules/services/gpg.nix
@@ -1,19 +1,20 @@
 { config, pkgs, lib, ... }:
-with lib;
-{
+with lib; {
   options.myHome.gpg.enable = mkOption {
-      type = types.bool;
-      default = true;
-    };
+    type = types.bool;
+    default = true;
+  };
 
   config = mkIf config.myHome.gpg.enable {
     services.gpg-agent = {
       enable = true;
-      pinentryFlavor = if config.myHome.desktop.enable then "gtk2" else "tty";
+      pinentryPackage = if config.myHome.desktop.enable then
+        pkgs.pinentry-gtk2
+      else
+        pkgs.pinentry-tty;
       defaultCacheTtl = 3600; # 1 hour
       maxCacheTtl = 28800; # 8 hours
     };
     home.packages = with pkgs; [ gnupg ];
   };
 }
-
