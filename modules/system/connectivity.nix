02727f1d2cd9abacb907a7fdc42a6e4ad09d5b90
Deniz Koluacik <koluacik@disroot.org>
2026-01-22 12:50:12 +0300

    remove wireguard config

--- modules/system/connectivity.nix
+++ modules/system/connectivity.nix
@@ -1,27 +1,10 @@
 { pkgs, config, lib, ... }:
 {
-  # see: nixos.wiki/wiki/WireGuard
-  networking.firewall = {
-    # if packets are still dropped, they will show up in dmesg
-    logReversePathDrops = true;
-    # wireguard trips rpfilter up
-    extraCommands = ''
-      ip46tables -t raw -I nixos-fw-rpfilter -p udp -m udp --sport 3389 -j RETURN
-      ip46tables -t raw -I nixos-fw-rpfilter -p udp -m udp --dport 3389 -j RETURN
-    '';
-    extraStopCommands = ''
-      ip46tables -t raw -D nixos-fw-rpfilter -p udp -m udp --sport 3389 -j RETURN || true
-      ip46tables -t raw -D nixos-fw-rpfilter -p udp -m udp --dport 3389 -j RETURN || true
-    '';
-  };
-
   hardware.bluetooth = {
     enable = true;
     package = pkgs.bluez;
     hsphfpd.enable = !config.services.pipewire.wireplumber.enable;
   };
-
-  environment.systemPackages = [ pkgs.networkmanager-fortisslvpn ];
 
   services.blueman.enable = true;
 
